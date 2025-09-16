e60bd23cb7f239d1f39bcfeec2bf140be7f7806b
Avery Lopez <averyagustin05@gmail.com>
2025-09-16 15:04:40 -0700

    Started structure for frontend components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import react from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, SudoSecure!</h1>
+      <h1>Hello, SudoSecure</h1>
       <p>Welcome to the SudoSecure application.</p>
     </div>
   );
