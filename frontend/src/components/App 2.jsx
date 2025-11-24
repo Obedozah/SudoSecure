738629e2b716482e684fffaad95c06a036740dda
Avery Lopez <averyagustin05@gmail.com>
2025-11-24 15:28:27 -0800

    weird

---
+++ frontend/src/components/App 2.jsx
@@ -0,0 +1,15 @@
+import Header from './HeaderFooter/Header';
+import FAQ from './HeaderFooter/FAQ';
+import PasswordSection from './PasswordSection/PasswordSection';
+
+function App() {
+    return (
+        <div className="app">
+            <Header />
+            <PasswordSection />
+            <FAQ />
+        </div>
+    );
+}
+
+export default App;
