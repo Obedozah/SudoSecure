eea12828e7449bd28535640e67a6d24e05502a81
pabloxbm <pablomantanic@gmail.com>
2025-12-02 14:44:15 -0800

    more faqs

--- frontend/src/components/HeaderFooter/FAQ.jsx
+++ frontend/src/components/HeaderFooter/FAQ.jsx
@@ -24,15 +24,23 @@ function FAQ() {
 
     const [q1Display, setQ1Visi] = useState('collapse');
     const [q1PlusDisplay, setQ1Plus] = useState("+");
+    const [q4Display, setQ4Visi] = useState('collapse');
+    const [q4PlusDisplay, setQ4Plus] = useState("+");
+    const [q5Display, setQ5Visi] = useState('collapse');
+    const [q5PlusDisplay, setQ5Plus] = useState("+");
     //MAKE DYNAMIC SPACING FOR FAQS
     const [q1expanded, setq1expand] = useState(false);
     const [q2expanded, setq2expand] = useState(false);
     const [q3expanded, setq3expand] = useState(false);
+    const [q4expanded, setq4expand] = useState(false);
+    const [q5expanded, setq5expand] = useState(false);
     const handleClickQ1 = ()=>{
         if(q1PlusDisplay==="+"){
             setQ1Plus("~")
             setQ2Plus("+")
             setQ3Plus("+")
+            setQ4Plus("+")
+            setQ5Plus("+")
         }else{
             setQ1Plus("+")
         }
@@ -40,12 +48,16 @@ function FAQ() {
             setQ1Visi('visible');
             setQ2Visi('collapse');
             setQ3Visi('collapse');
+            setQ4Visi('collapse');
+            setQ5Visi('collapse');
         }else{
             setQ1Visi('collapse');
         }
         setq1expand(!q1expanded);
         setq2expand(false);
         setq3expand(false);
+        setq4expand(false);
+        setq5expand(false);
         //Display moving
         // if(q2DisplayTOP === '120px'){
         //     setq2TOP('280px')
@@ -59,6 +71,8 @@ function FAQ() {
         // }
     }
     const [q1Hover] = useState("default-border");
+    const [q4Hover] = useState("default-border");
+    const [q5Hover] = useState("default-border");
 
 
     const [q2Display, setQ2Visi] = useState('collapse');
@@ -68,6 +82,8 @@ function FAQ() {
             setQ2Plus("~")
             setQ3Plus("+")
             setQ1Plus("+")
+            setQ4Plus("+")
+            setQ5Plus("+")
         }else{
             setQ2Plus("+")
         }
@@ -75,12 +91,16 @@ function FAQ() {
             setQ2Visi('visible');
             setQ1Visi('collapse');
             setQ3Visi('collapse');
+            setQ4Visi('collapse');
+            setQ5Visi('collapse');
         }else{
             setQ2Visi('collapse');
         }
         setq2expand(!q2expanded);
         setq1expand(false);
         setq3expand(false);
+        setq4expand(false);
+        setq5expand(false);
         //Display moving
         // if(q2DisplayTOP === '280px'){
         //     setq2TOP('120px')
@@ -101,6 +121,8 @@ function FAQ() {
             setQ3Plus("~")
             setQ2Plus("+")
             setQ1Plus("+")
+            setQ4Plus("+")
+            setQ5Plus("+")
         }else{
             setQ3Plus("+")
         }
@@ -108,12 +130,71 @@ function FAQ() {
             setQ3Visi('visible');
             setQ2Visi('collapse');
             setQ1Visi('collapse');
+            setQ4Visi('collapse');
+            setQ5Visi('collapse');
         }else{
             setQ3Visi('collapse');
         }
         setq3expand(!q3expanded);
         setq2expand(false);
         setq1expand(false);
+        setq4expand(false);
+        setq5expand(false);
+        // setq3TOP('140px')
+        // setq2TOP('120px')
+    }
+    
+    const handleClickQ4 = ()=>{
+        if(q4PlusDisplay==="+"){
+            setQ4Plus("~")
+            setQ2Plus("+")
+            setQ1Plus("+")
+            setQ3Plus("+")
+            setQ5Plus("+")
+        }else{
+            setQ4Plus("+")
+        }
+        if(q4Display==='collapse'){
+            setQ4Visi('visible');
+            setQ2Visi('collapse');
+            setQ1Visi('collapse');
+            setQ3Visi('collapse');
+            setQ5Visi('collapse');
+        }else{
+            setQ4Visi('collapse');
+        }
+        setq4expand(!q4expanded);
+        setq2expand(false);
+        setq1expand(false);
+        setq3expand(false);
+        setq5expand(false);
+        // setq3TOP('140px')
+        // setq2TOP('120px')
+    }
+    const handleClickQ5 = ()=>{
+        if(q5PlusDisplay==="+"){
+            setQ5Plus("~")
+            setQ2Plus("+")
+            setQ1Plus("+")
+            setQ4Plus("+")
+            setQ3Plus("+")
+        }else{
+            setQ5Plus("+")
+        }
+        if(q5Display==='collapse'){
+            setQ5Visi('visible');
+            setQ2Visi('collapse');
+            setQ1Visi('collapse');
+            setQ4Visi('collapse');
+            setQ3Visi('collapse');
+        }else{
+            setQ5Visi('collapse');
+        }
+        setq5expand(!q5expanded);
+        setq2expand(false);
+        setq1expand(false);
+        setq4expand(false);
+        setq3expand(false);
         // setq3TOP('140px')
         // setq2TOP('120px')
     }
@@ -123,14 +204,14 @@ function FAQ() {
     const [q3Hover] = useState("default-border");
 
     return (
-    <header style={{background: 'transparent'}}>
+    <header style={{background: 'linear-gradient(to bottom, rgba(48, 51, 58, .5), rgba(34, 37, 44, 0.5))'}}>
       {/* ===== GOOD PRACTICES ===== */}
       <h1 id="GoodP" className="goodp-section">
         <b className="goodp-title">Good Practices</b>
         <div className="goodp-sub">
           <div className="goodp-image">
             <img className={`goodp-img ${img1Hover}`} src={'show.png'} alt='img 1'></img>
-            <p>Easy To Remember Password</p>
+            <p>Use Complex Passwords</p>
           </div>
           <div className="goodp-image">
             <img className='goodp-img' src={'show.png'} alt='img 2'></img>
@@ -138,21 +219,21 @@ function FAQ() {
           </div>
           <div className="goodp-image">
             <img className='goodp-img' src={'show.png'} alt='img 3'></img>
-            <p>3rd</p>
+            <p>Don't Reuse Passwords</p>
           </div>
         </div>
         <div className="goodp-sub">
           <div className="goodp-image">
             <img className='goodp-img' src={'show.png'} alt='img 4'></img>
-            <p>4th</p>
+            <p>Don't Let Browsers Save Your Passwords</p>
           </div>
           <div className="goodp-image">
             <img className='goodp-img' src={'show.png'} alt='img 5' ></img>
-            <p>5th</p>
+            <p>Don't Share Passwords</p>
           </div>
           <div className="goodp-image">
             <img className='goodp-img' src={'show.png'} alt='img 6'></img>
-            <p>6th</p>
+            <p>Physical Instead of Digital Copies</p>
           </div>
         </div>
       </h1>
@@ -162,7 +243,7 @@ function FAQ() {
         id="FAQ"
         className="faq-section"
         style={{
-          height: zoomLevel < 300 ? "550px" : "900px",
+          height: zoomLevel < 300 ? "600px" : "900px",
         }}
       >
         <b
@@ -247,6 +328,56 @@ function FAQ() {
                 className="faq-answer"
                 style={{
                   visibility: q3Display,
+                }}
+              >
+                Apple Bannana Cucumber Desert Emmett Faraway Great High-five Internet Joe
+                  Karat Lemons Minnetsota. Niagra Falls Orangutan Prerequisite Quintanilla 
+                  Rusty Southern Tires Umbrella Video Wires Xylophone Young Zootopia.
+            </p>
+          </section>
+        
+        </div>
+        <div className="faq-questions-section">
+          {/* ---------------- Q4 ---------------- */}
+          <section className={`faq-row ${q4expanded ? "expanded" : ""}`}>
+            <button
+              onClick={handleClickQ4}
+              className={`faq-btn ${q4Hover}`}
+              // style={{  height: zoomLevel < 400 ? "40px" : "75px", }}
+            >
+              CFAQ EUSTION 4?
+              <p className="faq-plus">{q4PlusDisplay}</p>
+
+            </button>
+            <p
+                className="faq-answer"
+                style={{
+                  visibility: q4Display,
+                }}
+              >
+                Apple Bannana Cucumber Desert Emmett Faraway Great High-five Internet Joe
+                  Karat Lemons Minnetsota. Niagra Falls Orangutan Prerequisite Quintanilla 
+                  Rusty Southern Tires Umbrella Video Wires Xylophone Young Zootopia.
+            </p>
+          </section>
+        
+        </div>
+        <div className="faq-questions-section">
+          {/* ---------------- Q5 ---------------- */}
+          <section className={`faq-row ${q5expanded ? "expanded" : ""}`}>
+            <button
+              onClick={handleClickQ5}
+              className={`faq-btn ${q5Hover}`}
+              // style={{  height: zoomLevel < 400 ? "40px" : "75px", }}
+            >
+              FAQ QUESTION 5d?
+              <p className="faq-plus">{q5PlusDisplay}</p>
+
+            </button>
+            <p
+                className="faq-answer"
+                style={{
+                  visibility: q5Display,
                 }}
               >
                 Apple Bannana Cucumber Desert Emmett Faraway Great High-five Internet Joe
