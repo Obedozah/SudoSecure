f2be0f622188ab671795b770b4c1611748a0952e
Avery Lopez <averyagustin05@gmail.com>
2025-12-04 14:57:11 -0800

    Changed Naming and ico

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,7 +6,6 @@ import './App.css'
 function App() {
     return (
         <div className="app">
-            <title>SudoSecure</title>
             <header>
                 <Header />
             </header>
