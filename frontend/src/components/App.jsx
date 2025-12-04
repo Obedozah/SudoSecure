aa6d24346b4053c9877c3279fccfe8ec68d2877f
Avery Lopez <averyagustin05@gmail.com>
2025-12-04 14:44:16 -0800

    Added Title

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
     return (
         <div className="app">
+            <title>SudoSecure</title>
             <header>
                 <Header />
             </header>
