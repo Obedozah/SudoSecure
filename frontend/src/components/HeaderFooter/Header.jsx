2f816ede930f5622ac3764efed79c090ceef1b1e
Avery Lopez <averyagustin05@gmail.com>
2025-12-04 13:58:28 -0800

    Merged with pablos

--- frontend/src/components/HeaderFooter/Header.jsx
+++ frontend/src/components/HeaderFooter/Header.jsx
@@ -13,18 +13,34 @@ function Header() {
     window.addEventListener("resize", calculateZoomLevel);
     return () => window.removeEventListener("resize", calculateZoomLevel);
   }, []);
+//==========================
+//CHECK WINDOW MODE
+//==========================
+  const[winWidth, setWinWidth] = useState(2560);
+  const [winlength, setWinLength] = useState(window.outerWidth);
+
+  useEffect(() => {
+    const calculateWindowWidth = () => {
+      setWinWidth(window.innerWidth);
+      setWinLength(window.outerWidth);
+    };
+
+    calculateWindowWidth();
+    window.addEventListener("resize", calculateWindowWidth);
+    return() => window.removeEventListener("resize", calculateWindowWidth);
+  }, []);
 
   // Dynamic styles that *must* remain in JS
   const logoStyle = {
     height: "75px",
     position: "absolute",
     top: "23px",
-    left:
+    left: (winWidth===winlength|| winWidth>winlength+616)?
       zoomLevel < 175
         ? "330px"
         : zoomLevel <= 250
         ? "120px"
-        : "-5px",
+        : "-5px":'-5px',
   };
 
   const titleStyle = {
@@ -33,24 +49,27 @@ function Header() {
     transform: "skew(-6deg)",
     position: "absolute",
     top: "35px",
-    left:
+    left: (winWidth===winlength|| winWidth>winlength+616)?
       zoomLevel < 175
         ? "388px"
         : zoomLevel <= 250
         ? "178px"
-        : "53px",
+        : "53px":'53px',
   };
 
+  // const updateWinLen=()=>{
+  //   setWinLen(Math.round(window.innerWidth*100));
+  // }
   const btnCommon = {
     backgroundColor: "#333",
     color: "#fff",
     border: "0",
     cursor: "pointer",
     position: "absolute",
-    height: zoomLevel <= 250 ? "65px" : "45px",
-    width: zoomLevel <= 250 ? "150px" : "105px",
-    fontSize: zoomLevel <= 250 ? "18px" : "13px",
-    top: zoomLevel <= 250 ? "35px" : "45px",
+    height: (winWidth===winlength|| winWidth>winlength+616)? zoomLevel <= 250 ? "65px" : "45px":'45px',
+    width: (winWidth===winlength|| winWidth>winlength+616)? zoomLevel <= 250 ? "150px" : "105px":'105px',
+    fontSize: (winWidth===winlength|| winWidth>winlength+616)? zoomLevel <= 250 ? "18px" : "13px":'13px',
+    top: (winWidth===winlength|| winWidth>winlength+616)? (zoomLevel <= 250 ? "35px" : "45px") : ((zoomLevel<110)? '45px': (winWidth+1900<winlength)? '45px': ((winWidth>=1800&&winlength>=1800)||winWidth>=500)? '45px':'100px'),
   };
 
   return (
@@ -63,8 +82,10 @@ function Header() {
         />
 
         <i style={titleStyle}>
+          {/* {window.outerWidth} window.lengt{winWidth} */}
           sudo<b>Secure</b>
         </i>
+        {/* <button onClick={updateWinLen}>Bro</button> */}
 
         <button
           onClick={() => {
@@ -72,12 +93,12 @@ function Header() {
           }}
           style={{
             ...btnCommon,
-            right:
+            right: (winWidth===winlength|| winWidth>winlength+616)?
               zoomLevel < 175
                 ? "515px"
                 : zoomLevel <= 250
                 ? "305px"
-                : "105px",
+                : "105px": '105px',
             borderTopLeftRadius: "9px",
             borderBottomLeftRadius: "9px",
           }}
@@ -93,12 +114,12 @@ function Header() {
           }}
           style={{
             ...btnCommon,
-            right:
+            right: (winWidth===winlength|| winWidth>winlength+616)?
               zoomLevel < 175
                 ? "365px"
                 : zoomLevel <= 250
                 ? "155px"
-                : "0px",
+                : "0px": "0px",
             borderTopRightRadius: "9px",
             borderBottomRightRadius: "9px",
           }}
